1e859cd2afc742caaf22fc4e97a0a24c2fea6a8d
yarensari <sariyaren221@gmail.com>
2025-04-20 18:01:33 +0300

    10_04_dinamikBellekYonetimi eklendi.

---
+++ 10_dinamikBellekYonetimi/10_04_dinamikBellekYonetimi/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+	calloc:clear and allocation=>yine yer ayirma islemi yapiliyor fakat bu sefer ayrilan yerlerin hepsine 0 degeri ataniyor.istenirse bu deger degistirilebilir.
+	realloc:reallocation=>daha onceden,calloc veya malloc ile, ayrilan yerin degerini arttirmayi saglar.
+	
+*/
+int main()
+{
+	int elemanSayisi;
+	int *dizi,*ptr;
+	//CALLOC
+	puts("Lutfen eleman sayisini giriniz:");
+	scanf("%d",&elemanSayisi);
+	dizi=(int *)calloc(elemanSayisi,sizeof(int));
+	
+	for(ptr=dizi;ptr<dizi+elemanSayisi;ptr++)
+	{
+		*ptr=10;
+		printf("%d ",*ptr);
+	}
+	//REALLOC
+	puts("\nEleman sayisini kaca yukselteceksiniz:");
+	scanf("%d",&elemanSayisi);
+	
+	dizi=realloc(dizi,elemanSayisi*sizeof(int));	//elemanSayisi yukseltildi.artan elemanSayisi degerinden sonra artik o elemanlarin yerine garbage value'lar geldi
+	
+	for(ptr=dizi;ptr<dizi+elemanSayisi;ptr++)
+	{
+		*ptr=20;	//garbage value olmasin diye
+		printf("%d ",*ptr);
+	}
+	
+	return 0;
+}
